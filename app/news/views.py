22653251ec8c673c0e5d3e8d7a8cc839a916de4a
didiotghima <nurlanuuulubeksultan@gmail.com>
2024-12-11 09:29:12 +0600

    add principl SOLID news

--- app/news/views.py
+++ app/news/views.py
@@ -1,39 +1,39 @@
-from django.shortcuts import render, redirect
-from app.news.models import NewsPage, NewsListDetails, NewsComment
+from django.shortcuts import redirect
 from django.views.generic import TemplateView
-from django.core.paginator import Paginator
+from app.news.services import NewsService
 
 class NewsView(TemplateView):
     template_name = 'news/news.html'
 
     def get_context_data(self, **kwargs):
+        """Передача данных для отображения списка новостей."""
         context = super().get_context_data(**kwargs)
-        context['news_id'] = NewsPage.objects.latest("id")  # Берем последнюю новость
-        context['news_list_all'] = NewsListDetails.objects.all()  # Все новости
+        context['news_id'] = NewsService.get_latest_news()
+        context['news_list_all'] = NewsService.get_all_news()
         return context
 
 class NewsDetailsView(TemplateView):
     template_name = 'news/news-details.html'
 
     def get_context_data(self, **kwargs):
+        """Передача данных для отображения деталей новости и комментариев."""
         context = super().get_context_data(**kwargs)
-        news = NewsListDetails.objects.get(id=kwargs['id'])
-        context['news'] = news
-        comments = NewsComment.objects.filter(news=news)
-        paginator = Paginator(comments, 5)
+        news = NewsService.get_news_details(kwargs['id'])
         page_number = self.request.GET.get('page')
-        page_obj = paginator.get_page(page_number)
+        page_obj = NewsService.get_paginated_comments(news, page_number)
 
+        context['news'] = news
         context['news_comment'] = page_obj
         context['page_obj'] = page_obj
         return context
 
     def post(self, request, *args, **kwargs):
+        """Обработка формы комментариев."""
         name = request.POST.get('name')
         email = request.POST.get('email')
         comment = request.POST.get('message')
 
         if name and email and comment:
-            NewsComment.objects.create(name=name, email=email, comment=comment, news_id=kwargs['id'])
+            NewsService.create_comment(name, email, comment, kwargs['id'])
 
         return redirect('news:news-detail', id=kwargs['id'])
