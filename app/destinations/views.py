f929d3950c167eacfc1eabd56499ab03e03a2ab9
didiotghima <nurlanuuulubeksultan@gmail.com>
2024-12-21 16:18:35 +0600

    commit

--- app/destinations/views.py
+++ app/destinations/views.py
@@ -2,7 +2,6 @@ from django.http import Http404
 from django.views.generic import TemplateView
 from app.destinations.services import DestinationsService
 
-
 class DestinationsView(TemplateView):
     template_name = 'destinations/destinations.html'
 
@@ -22,17 +21,13 @@ class DestinationsDetailsView(TemplateView):
             destination = DestinationsService.get_destination_by_id(kwargs['id'])
             if not destination:
                 raise Http404("Направление не найдено")
-
             destination_details = DestinationsService.get_destination_details_by_destination(destination)
             destination_details_pynkt = DestinationsService.get_all_destinationspynkt()
-
             context.update({
                 'destination_id': destination,
                 'destination_details': destination_details,
                 'destination_details_pynkt': destination_details_pynkt,
             })
-
         except Exception as e:
             context['error'] = str(e)
-
         return context
